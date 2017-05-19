2436899a4fec93aea459632ad2c7561d763731e2
valrat <leri.ratu@gmail.com>
2017-05-18 20:42:03 -0700

    Manually hijacked altitude to not give NaNs

--- src/libnmea_navsat_driver/driver.py
+++ src/libnmea_navsat_driver/driver.py
@@ -118,7 +118,7 @@ class RosNMEADriver(object):
                 NavSatFix.COVARIANCE_TYPE_APPROXIMATED
 
             # Altitude is above ellipsoid, so adjust for mean-sea-level
-            altitude = data['altitude'] + data['mean_sea_level']
+            altitude = 0 # data['altitude'] + data['mean_sea_level']
             current_fix.altitude = altitude
 
             self.fix_pub.publish(current_fix)
